af950758a1d2da79b54bfac6c62710e14f0987ef
Maciej Gamrot <maciekgamro@gmail.com>
2020-02-13 11:05:40 +0100

    First Flight

--- RunModes.py
+++ RunModes.py
@@ -18,7 +18,7 @@ class LiveFlight():
         self.threadpool = QThreadPool()
         self.log = LiveFlightLogic(self)
         # Communication Thread
-        self.ser = SerialCommunicator('COM10')
+        self.ser = SerialCommunicator('COM7')
         self.ser.setOnReadCallback(self.log.newDataCallback)
         self.threadpool.start(self.ser)
         # main window
@@ -32,33 +32,75 @@ class LiveFlightLogic(QRunnable):
     def __init__(self, parent):
         super().__init__()
         self.parent = parent
-        self.left_motor = 0
-        self.right_motor = 0
+        self.left_motor = 50
+        self.right_motor = 50
+        self.left_key = False
+        self.right_key = False
+        self.left_key = False
+        self.up_key = False
+        self.down_key = False
+        self.power = 0
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Left:
-            self.left_motor = 1
-        if event.key() == Qt.Key_Right:
-            self.right_motor = 1
+            self.left_key = True
+        elif event.key() == Qt.Key_Right:
+            self.right_key = True
+        elif event.key() == Qt.Key_Up:
+            self.up_key = True
+        elif event.key() == Qt.Key_Down:
+            self.down_key = True
 
     def keyReleaseEvent(self, event):
         if event.key() == Qt.Key_Left:
-            self.left_motor = 0
-        if event.key() == Qt.Key_Right:
-            self.right_motor = 0
+            self.left_key = False
+        elif event.key() == Qt.Key_Right:
+            self.right_key = False
+        elif event.key() == Qt.Key_Up:
+            self.up_key = False
+        elif event.key() == Qt.Key_Down:
+            self.down_key = False
 
     @pyqtSlot()
     def run(self):
         self.parent.main_window.keyPressEvent = self.keyPressEvent
         self.parent.main_window.keyReleaseEvent = self.keyReleaseEvent
+        step = 5
         while True:
-            line = '{}{}\n'.format(self.left_motor, self.right_motor)
-            self.parent.ser.writeline(bytes(line, encoding='utf8'))
-            print(self.parent.mm.dm.get_last(3))
-            time.sleep(0.1)
+            if self.left_key and self.left_motor<=100-step:
+                self.left_motor+=step
+                self.right_motor-=step
+            if self.right_key and self.right_motor<=100-step:
+                self.right_motor+=step
+                self.left_motor-=step
+            if self.up_key and self.power<=100-step:
+                self.power +=step
+            if self.down_key and self.power>=step:
+                self.power -= step
+
+            temp_text = 'l:{} r:{} p:{}'.format(
+            self.left_motor, self.right_motor, self.power)
+            self.parent.main_window.temp2.setText(temp_text)
+
+
+            #line = '{}{}\n'.format(self.left_motor, self.right_motor)
+            #self.parent.ser.writeline(bytes(line, encoding='utf8'))
+            l_out = int(self.left_motor*self.power/100)*2
+            r_out = int(self.right_motor*self.power/100)*2
+            print(r_out, l_out)
+            motbytes = bytes([0xFF, l_out, r_out])
+            #print(motbytes)
+            try:
+                self.parent.ser.writeline(motbytes)
+            except Exception as e:
+                pass
+            #print(self.parent.mm.dm.get_last(3))
+            time.sleep(0.001)
+
 
     def newDataCallback(self, data):
         self.parent.main_window.temp.setText(data)
+        #print(data)
         self.parent.mm.dm.append(data)
 
 lf = LiveFlight()
